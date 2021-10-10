ea85f8755aa61f2d4577c14b1f3a6bc803b58065
SerSu <sersurgut88@yandex.ru>
2021-10-10 08:40:52 +0300

    Added style and Scheme for dataset

--- src/components/DataSetView/DataSetView.js
+++ src/components/DataSetView/DataSetView.js
@@ -1,32 +1,37 @@
 import React from 'react';
-import { Tabs } from 'antd';
+import {Tabs} from 'antd';
 import {Attributes} from "../Attributes/Attributes";
+import styles from './DataSetView.module.css'
+
 
 function DataSetView({dataSet}) {
-  const { TabPane } = Tabs;
+    const {TabPane} = Tabs;
 
-  return (
-    <div>
-      <h1>{ dataSet.title }</h1>
-      <div> Descriptions </div>
-      <div>
-        <Tabs defaultActiveKey="1" onChange={null}>
-          <TabPane tab="Схема" key="1">
-            <div>Название полей и тип данных</div>
-          </TabPane>
-            <TabPane tab="Атрибуты" key="2">
-                <Attributes/>
-            </TabPane>
-          <TabPane tab="Связи" key="3">
-            <div>Список с какими датасетами этот датасет связан</div>
-          </TabPane>
-          <TabPane tab="Статистика" key="4">
-            <div>2 графика</div>
-          </TabPane>
-        </Tabs>
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <h1 className={styles.title}>{dataSet.title}</h1>
+                <div className={styles.description}> Descriptions</div>
+            </div>
+            <div className={styles.tabs}>
+                <Tabs defaultActiveKey="1" onChange={null}>
+                    <TabPane tab="Схема" key="1">
+                        <pre>
+                            <code>
+                                {JSON.stringify(dataSet).replaceAll(',', '\n')}
+                            </code>
+                        </pre>
+                    </TabPane>
+                    <TabPane tab="Связи" key="3">
+                        <div>Список с какими датасетами этот датасет связан</div>
+                    </TabPane>
+                    <TabPane tab="Статистика" key="4">
+                        <div>2 графика</div>
+                    </TabPane>
+                </Tabs>
+            </div>
+        </div>
+    );
 }
 
 export default DataSetView;
