a7aa53adfd32dfc795e890c3a350fa4efa882612
Katrin <alimovaea@sibur.ru>
2021-10-10 08:50:38 +0300

    dataset view

--- src/components/DataSetView/DataSetView.js
+++ src/components/DataSetView/DataSetView.js
@@ -1,32 +1,65 @@
-import React from 'react';
-import { Tabs } from 'antd';
-import {Attributes} from "../Attributes/Attributes";
+import React, {useState} from 'react';
+import {Button, Divider, Space, Tabs, Tag} from 'antd';
+import Avatar from "antd/es/avatar/avatar";
+
+function DollarOutlined() {
+    return null;
+}
+
+function PoweroffOutlined() {
+    return null;
+}
+
+function UserOutlined() {
+    return null;
+}
 
 function DataSetView({dataSet}) {
-  const { TabPane } = Tabs;
+    const {TabPane} = Tabs;
+    const [btnBuy, setBtnBuy] = useState(false);
+    const [tags, setTags] = useState(['продажи','sales','SAP', 'отдел продаж'])
+    const [colors, setColors] = useState(['magenta','volcano','cyan', 'blue'])
+    const handlerBtnBuy = () => {
+        setBtnBuy(true);
+        setTags(prev => [...prev, 'куплено']);
+        setColors(prev => [...prev, 'green'])
+    };
 
-  return (
-    <div>
-      <h1>{ dataSet.title }</h1>
-      <div> Descriptions </div>
-      <div>
-        <Tabs defaultActiveKey="1" onChange={null}>
-          <TabPane tab="Схема" key="1">
-            <div>Название полей и тип данных</div>
-          </TabPane>
-            <TabPane tab="Атрибуты" key="2">
-                <Attributes/>
-            </TabPane>
-          <TabPane tab="Связи" key="3">
-            <div>Список с какими датасетами этот датасет связан</div>
-          </TabPane>
-          <TabPane tab="Статистика" key="4">
-            <div>2 графика</div>
-          </TabPane>
-        </Tabs>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <h1>{dataSet.title}</h1>
+            <div> Descriptions</div>
+            499 RUB
+            <Space style={{width: '100%'}}>
+                {btnBuy ? null : <Button
+                    type="green"
+                    icon={<PoweroffOutlined/>}
+                    onClick={handlerBtnBuy}>
+                    Купить
+                </Button>}
+            </Space>
+            <div>
+                Владелец
+                <Avatar size={64} src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>
+Семен Игоревич
+            </div>
+
+            <Divider orientation="left">Теги</Divider>
+            <div>
+                {tags.map((tag,i) => <Tag key={tag} color={[colors[i]]}>{tag}</Tag>)}
+            </div>
+            <div>
+                <Tabs defaultActiveKey="1" onChange={null}>
+                    <TabPane tab="Схема" key="1">
+                        <div>Название полей и тип данных</div>
+                    </TabPane>
+                    <TabPane tab="Связи" key="3">
+                        <div>Список с какими датасетами этот датасет связан</div>
+                    </TabPane>
+                </Tabs>
+            </div>
+        </div>
+    );
 }
 
 export default DataSetView;
